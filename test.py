d264c2b3aa02b02b32f88a1c50321423ec533567
valentinoli <valentin.loftsson@gmail.com>
2021-05-26 15:30:47 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,59 +1,37 @@
-from network import *
 import dlc_practical_prologue as prologue
+from network import NaiveNet, SharedWeightNet
+from helpers import train_model, compute_accuracy
 
-def train_and_test_model(model, N, mini_batch_size, epochs, lambda_, learning_rate, auxiliary_loss=False):
-	#generate train/test set
-	train_input, train_target, train_classes, \
-	test_input, test_target, test_classes = \
-	prologue.generate_pair_sets(N)
+num_samples = 1000
 
-	if auxiliary_loss:
-		#train model using auxiliary loss
-		train_model(model, train_input, train_target, train_classes, learn_rate_= learning_rate,
-					lambda_=lambda_, mini_batch_size=mini_batch_size, nb_epochs = epochs)
-	else:
-		train_model(model, train_input, train_target, learn_rate_= learning_rate, lambda_=0,
-			mini_batch_size=mini_batch_size, nb_epochs = epochs)
+# auxiliary loss parameter
+lambda_ = .23  
 
-	#count errors
-	train_error = compute_nb_errors(model, train_input, train_target, mini_batch_size)
-	test_error = compute_nb_errors(model, test_input, test_target, mini_batch_size)
-	
-	return train_error, test_error
+(train_input, train_target, train_classes,
+ test_input, test_target, test_classes) = prologue.generate_pair_sets(num_samples)
 
-def print_acc(model_name, train_error, test_error, N):
-	print(f'{model_name}\n\
-\ttraining set accurary : {100-train_error/N*100}%\n\
-\ttest set accuracy : {100-test_error/N*100}%\n')
+# We test four types of nets
+titles = [
+    'Naive network, no weight sharing, 1 hidden layer',
+    'Siamese network with weight sharing, 1 hidden layer',
+    'Siamese network with weight sharing, 1 hidden layer and auxiliary loss',
+    'Siamese network with weight sharing, 2 hidden layers and auxiliary loss'
+]
+models = [NaiveNet(), SharedWeightNet(), SharedWeightNet(), SharedWeightNet(hidden_layers=2)]
+learning_rates = [1.6e-3, 1.6e-3, 3.1e-3, 1.2e-2]
+with_aux_loss = [False, False, True, True]
 
-#params
-N=1000
-epochs=25
+for title, model, lr, auxiliary_loss in zip(titles, models, learning_rates, with_aux_loss):
+    print(title)
+    # Train the model using the default number of epochs and batch size
+    train_model(model, train_input, train_target, train_classes, learning_rate=lr, lambda_=lambda_, auxiliary_loss=auxiliary_loss)
 
-#hyper-parameters
-mini_batch_size = 100
-lambda_ = .23
+    # Compute train and test accuracy
+    train_accuracy, _ = compute_accuracy(model, train_input, train_target)
+    test_accuracy, _ = compute_accuracy(model, test_input, test_target)
 
-#Naive network, no weight sharing, 1 hidden layer
-Naive = NaiveCNN(1)
-Naive_name = "naive network (1 hidden)"
-tr_e, te_e = train_and_test_model(Naive, N, mini_batch_size, epochs, lambda_, learning_rate=1.6e-3, auxiliary_loss=False)
-print_acc(Naive_name, tr_e, te_e, N)
-
-#Siamese network, 1 hidden layer
-Siamese_1h = SharedWeight(1)
-Siamese_1h_n = "siamese network (1 hidden)"
-tr_e, te_e = train_and_test_model(Siamese_1h, N, mini_batch_size, epochs, lambda_, learning_rate=1.6e-3, auxiliary_loss=False)
-print_acc(Siamese_1h_n, tr_e, te_e, N)
-
-#Siamese network, 1 hidden layer with auxi loss
-Siamese_1h = SharedWeight(1)
-Siamese_1h_n = "siamese network (1 hidden) with auxi. loss"
-tr_e, te_e = train_and_test_model(Siamese_1h, N, mini_batch_size, epochs, lambda_, learning_rate=3.1e-3, auxiliary_loss=True)
-print_acc(Siamese_1h_n, tr_e, te_e, N)
-
-#Siamese network, 2 hidden layer
-Siamese_2h = SharedWeight(2)
-Siamese_2h_n = "siamese network (2 hidden) with auxi. loss"
-tr_e, te_e = train_and_test_model(Siamese_2h, N, mini_batch_size, epochs, lambda_, learning_rate=1.2e-2, auxiliary_loss=True)
-print_acc(Siamese_2h_n, tr_e, te_e, N)
+    print(
+        'Training accuracy: {:.2f}%\nTest accuracy:     {:.2f}%\n'
+        .format(train_accuracy * 100, test_accuracy * 100)
+    )
+    print('#######################\n')
