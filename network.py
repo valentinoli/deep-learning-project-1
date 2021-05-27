a514ef29a8607f6bd55459b6794cdfe7f029c787
valentinoli <valentin.loftsson@gmail.com>
2021-05-27 21:04:58 +0200

    Create Net class

--- network.py
+++ network.py
@@ -1,17 +1,31 @@
+from __future__ import annotations
+
 import torch
-from torch import nn
+from torch import nn, Tensor
 from network_helpers import block_cnn, block_digit_classifier, block_output, leq
 
-class NaiveNet(nn.Module):
-    """Naive implementation without shared weights"""
-    def __init__(self, hidden_layers: int = 1):
-        super().__init__()        
+
+class Net(nn.Module):
+    def __init__(self, hidden_layers: int):
+        super().__init__()
+        # CNN block
         self.cnn = block_cnn()
         
+        # Classification blocks
         self.dc1 = block_digit_classifier(hidden_layers)
         self.dc2 = block_digit_classifier(hidden_layers)
         
+        # Output block
         self.out = block_output()
+        
+    def forward(self, input_: Tensor) -> tuple[Tensor, Tensor, Tensor]:
+        raise NotImplementedError
+
+
+class NaiveNet(Net):
+    """Naive implementation without shared weights"""
+    def __init__(self, hidden_layers: int = 1):
+        super().__init__(hidden_layers)
 
     def forward(self, input_):
         # Pass each image of every pair through the convolutional block
@@ -28,27 +42,26 @@ class NaiveNet(nn.Module):
         return output, output1, output2
 
 
-class SharedWeightNet(nn.Module):
+class SharedWeightNet(Net):
     """Network with shared weights"""
     def __init__(self, hidden_layers: int = 1):
-        super().__init__()
-        self.cnn = block_cnn()
-        
-        # Shared weight block
-        self.dc = block_digit_classifier(hidden_layers)
-        
-        self.out = block_output()
+        super().__init__(hidden_layers)
 
-    def forward_one(self, x):
+    def forward_one(self, input_: Tensor):
         """Forwards the one image of each pair through the CNN and classification blocks"""
-        return self.dc(self.cnn(x).flatten(1))
+        return self.dc1(self.cnn(input_).flatten(1))
+    
+    def forward_both(self, input_: Tensor):
+        """Forwards both images through the shared weight net"""
+        output1 = self.forward_one(input_.select(1, 0).unsqueeze(1))
+        output2 = self.forward_one(input_.select(1, 1).unsqueeze(1))
+        return output1, output2
     
     def forward(self, input_):
         """Forward pass of the input batch of image pairs"""
         # Forward both images in every pair through the first two blocks
         # -> shared weight
-        output1 = self.forward_one(input_.select(1, 0).unsqueeze(1))
-        output2 = self.forward_one(input_.select(1, 1).unsqueeze(1))
+        output1, output2 = forward_both(input_)
         
         # Computes the final output by concatenating the intermediate outputs of each pair 
         output = self.out(torch.cat((output1, output2), 1))
@@ -62,8 +75,7 @@ class BenchmarkNet(SharedWeightNet):
         
     def forward(self, input_):
         """Forward pass of the input batch of image pairs"""
-        output1 = self.forward_one(input_.select(1, 0).unsqueeze(1))
-        output2 = self.forward_one(input_.select(1, 1).unsqueeze(1))
+        output1, output2 = forward_both(input_)
         
         # Compute the output binary distribution by directly computing
         # if the most likely digit 1 is lesser or equal to the most likely digit 2
