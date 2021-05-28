acd6147a7b25f723c4b6613195393564a6f3134f
haeeunkim <haeeun.kim@epfl.ch>
2021-05-28 14:40:55 +0200

    bootstrapping

--- test.py
+++ test.py
@@ -5,13 +5,7 @@ from helpers import train_model, compute_accuracy
 
 num_samples = 1000
 
-# auxiliary loss parameter
-lambda_ = .23  
 
-train_input, train_target, train_classes, \
-test_input, test_target, test_classes = prologue.generate_pair_sets(num_samples)
-
-# We test five types of nets with appropriate hyper-parameters
 titles = [
     'Naive network, no weight sharing, 1 hidden layer',
     'Siamese network with weight sharing, 1 hidden layer',
@@ -38,22 +32,37 @@ hyper_parameters = [
 
 with_aux_loss = [False, False, True, True, True]
 
-for title, model, params, auxiliary_loss in zip(titles, models, hyper_parameters, with_aux_loss):
-    print(title)
-    for k, v in params.items():
-        print(f'{k}: {v}')
-    
-    # Train the model using the default number of epochs and batch size
-    train_model(model, train_input, train_target, train_classes, **params, auxiliary_loss=auxiliary_loss)
-    
-    sys.stdout.write('\rTraining complete!\n')
+agg_train_results = {t:[] for t in titles}
+agg_test_results = {t:[] for t in titles}
 
-    # Compute train and test accuracy
-    train_accuracy = compute_accuracy(model, train_input, train_target)
-    test_accuracy = compute_accuracy(model, test_input, test_target)
+for i in range(10):
+    train_input, train_target, train_classes, \
+    test_input, test_target, test_classes = prologue.generate_pair_sets(num_samples)
 
-    print(
-        'Training accuracy: {:.2f}%\nTest accuracy:     {:.2f}%\n'
-        .format(train_accuracy * 100, test_accuracy * 100)
-    )
-    print('#######################\n')
+    # We test five types of nets with appropriate hyper-parameters
+
+    for title, model, params, auxiliary_loss in zip(titles, models, hyper_parameters, with_aux_loss):
+        print(title)
+        for k, v in params.items():
+            print(f'{k}: {v}')
+
+        # Train the model using the default number of epochs and batch size
+        train_model(model, train_input, train_target, train_classes, **params, auxiliary_loss=auxiliary_loss)
+
+        sys.stdout.write('\rTraining complete!\n')
+
+        # Compute train and test accuracy
+        train_accuracy = compute_accuracy(model, train_input, train_target)
+        test_accuracy = compute_accuracy(model, test_input, test_target)
+        
+        agg_train_results[title].append(train_accuracy)
+        agg_test_results[title].append(test_accuracy)
+
+        
+        
+
+print(
+    'Training accuracy: {:.2f}%\nTest accuracy:     {:.2f}%\n'
+    .format(train_accuracy * 100, test_accuracy * 100)
+)
+print('#######################\n')
